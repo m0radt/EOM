dc1072f0dfe07480ff1b594d82a114e5c008e4aa
m0radt <tayamor@post.bgu.ac.il>
2025-09-25 19:30:45 +0300

    Update my_job_array.sh

--- my_job_array.sh
+++ my_job_array.sh
@@ -25,10 +25,10 @@ models_and_settings=(
     # WizardLMTeam/WizardCoder-Python-13B-V1.0  vllm       13.0             ""
     # meta-llama/CodeLlama-7b-Instruct-hf       hf         6.74             ""
     # meta-llama/CodeLlama-13b-Instruct-hf      hf         13.0             ""
-    # deepseek-ai/deepseek-coder-6.7b-instruct  hf         6.7              ""
+    deepseek-ai/deepseek-coder-6.7b-instruct  hf         6.7              ""
     # TheBloke/Phind-CodeLlama-34B-v1-GPTQ      vllm       34.0             ""
     # meta-llama/Llama-3.2-1B-Instruct          hf         1.24             ""
-    "mistralai/Mixtral-8x7B-Instruct-v0.1"     hf        46.7             ""
+    #"mistralai/Mixtral-8x7B-Instruct-v0.1"     hf        46.7             ""
 
 )
 
@@ -42,10 +42,19 @@ optional_flags=${models_and_settings[$((i + 3))]}
 
 
 temperature=0.0
-dataset="humaneval"
+dataset=humaneval
+validation_ids_path="splits/${dataset}_split_val_ids.jsonl"
+test_ids_path="splits/${dataset}_split_test_ids.jsonl"
+random_seed=42
+val_size=10
+embedding_dir="embeddings"
+validation_root="evalplus_validation_results"
+test_root="evalplus_test_results"
+
+
 echo "Running evaluation for model: $model, backend: $backend, parameters: $param_num B, dataset: $dataset , temperature: $temperature and flags: $optional_flags"
 module load anaconda                            ### load anaconda module (must be present when working with conda environments)
-source activate code_generation                 ### activate a conda environment.
+source activate eom                 ### activate a conda environment.
 
 export HUGGINGFACE_HUB_TOKEN=$(cat /home/tayamor/.hf_token)
 python -c "
@@ -60,10 +69,10 @@ else:
 "
 
 nvidia-smi 
-evalplus.evaluate --model $model --dataset $dataset --backend $backend  --greedy --device_map auto --trust_remote_code True $optional_flags
+python split_data.py --dataset $dataset --out_prefix splits/${dataset}_split --val_size $val_size --seed $random_seed
+python embed_model.py --model $model --dataset $dataset --backend $backend --param_num $param_num --embedding_dir $embedding_dir --validation_ids_path $validation_ids_path --root $validation_root $optional_flags
 # evalplus.evaluate --model $model --dataset $dataset --backend $backend  --temperature $temperature --device_map auto --trust_remote_code True $optional_flags
 nvidia-smi 
-python calculate_perplexity.py --model $model --param_num $param_num --backend $backend --temperature $temperature
 
 
 
@@ -77,41 +86,5 @@ python calculate_perplexity.py --model $model --param_num $param_num --backend $
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# export HUGGINGFACE_HUB_TOKEN=$(cat /home/tayamor/.hf_token)
-# python -c "
-# import os
-# from huggingface_hub import login
-
-# hf_token = os.getenv('HUGGINGFACE_HUB_TOKEN')
-# if hf_token:
-#     login(token=hf_token)
-# else:
-#     print('Warning: Hugging Face token not found. Gated models may not be accessible.')
-# "
-# python calculate_perplexity.py --model "meta-llama/Llama-3.2-1B-Instruct" --param_num 1.24 --backend hf --temperature 0.0 -r resultsSynth
-# python calculate_perplexity.py --model "meta-llama/CodeLlama-13b-Instruct-hf" --param_num 13.0 --backend hf --temperature 0.0 -r resultsSynth
-# python calculate_perplexity.py --model "meta-llama/CodeLlama-7b-Instruct-hf" --param_num 6.74 --backend hf --temperature 0.0 -r resultsSynth
-# python calculate_perplexity.py --model "deepseek-ai/deepseek-coder-6.7b-instruct" --param_num 6.7 --backend hf --temperature 0.0 -r resultsSynth
-# python calculate_perplexity.py --model "Salesforce/codegen-6B-multi" --param_num 6.0 --backend hf --temperature 0.0 -r resultsSynth
+python split_data.py --dataset humaneval --out_prefix splits/humaneval_split --val_size 10 --seed 42
+python embed_model.py --model deepseek-ai/deepseek-coder-6.7b-instruct --dataset humaneval --backend hf  --validation_ids_path splits/humaneval_split_val_ids.json --root evalplus_validation_results
